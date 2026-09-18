731228caa7e181f8d679976d3fddb06f828a739b
Yuuta <nguyenvantuanannghia@gmail.com>
2026-09-18 20:12:43 +0700

    chore: update base views to 1193

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ window.BIO_CONFIG = {
     avatar: "assets/avatar.jpeg",
     
     location: "saigon",
-    views: 794, // Lượt xem khởi đầu (base views)
+    views: 1193, // Lượt xem khởi đầu (base views)
     counterKey: "yuuta_bio_site_visits", // Mã lưu lượt xem trực tuyến trên Cloud Counter
     
     // Trích dẫn / tiểu sử
